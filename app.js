c28ba5ae3f1d6db65d64d3e4eb18d55ae0dc2d6c
Talhamalik495 <mohammadtalhamalik495@gmail.com>
2024-03-14 23:59:48 +0500

    legalillegal

---
+++ app.js
@@ -0,0 +1,16 @@
+
+
+// javascript legal veriables
+let nam1;
+let nam2_number_for;
+let $nam3;
+let _nam4;
+let nam5123;
+let nam8 = ("anas")
+let anas = ("ali")
+// javascript illegal veriables
+// let one the begguest;
+// let 1234two;
+// let tHree;
+// let five-in;
+// let let = 10;
